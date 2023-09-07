4d73b0f27dfe23f7461fc9fa3929549743e69964
Andrew Chow <40132839+acandrewchow@users.noreply.github.com>
2023-09-07 16:40:03 -0400

    Update run_reminder.sh

--- run_reminder.sh
+++ run_reminder.sh
@@ -4,5 +4,5 @@ while true; do
 
     cd /Users/andrewchow/Desktop/school_date_reminder/ # change to native directory
     
-    python3 reminder.py send_reminders.py 
+    python3 send_reminders.py
 done
